fee1e254e57523d735707ca898eb2190b5e956e2
GeovaneParedes <geovaneparedes@gmail.com>
2025-10-16 14:30:53 -0300

    "Refatorando o código de cálculo"

--- calculado.py
+++ calculado.py
@@ -1,4 +1,19 @@
-num_1 = 1
-num_2 = 4
-
-print(num_1 * num_2)
+while True:
+    num_1, num_2 = int(input("Digite um número: ", "Digite outro número: "))
+    operação = input("Digite a operação (+, -, *, /): ")
+    if operação == "+":
+        print(f"{num_1} + {num_2} = {num_1 + num_2}")
+    elif operação == "-":
+        print(f"{num_1} - {num_2} = {num_1 - num_2}")
+    elif operação == "*":
+        print(f"{num_1} * {num_2} = {num_1 * num_2}")
+    elif operação == "/":
+        if num_2 != 0:
+            print(f"{num_1} / {num_2} = {num_1 / num_2}")
+        else:
+            print("Erro: Divisão por zero não é permitida.")
+    else:
+        print("Operação inválida. Tente novamente.")
+    continuar = input("Deseja continuar? (s/n): ").lower()
+    if continuar != 's':
+        break
